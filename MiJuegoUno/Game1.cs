6bf8d0e9e168b19ef73df889d608c37962e57d4f
Susana <susana.diezsj@gmail.com>
2022-03-04 09:15:29 -0500

    Adding new folder Assets and image of spaceship and adding a new color and changing screen resolution

--- MiJuegoUno/Game1.cs
+++ MiJuegoUno/Game1.cs
@@ -9,17 +9,29 @@ namespace MiJuegoUno
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        byte red;
+        byte green;
+        byte blue;
+
+        Texture2D spaceShip;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            //_graphics.IsFullScreen = true;
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 600;
+            _graphics.ApplyChanges();
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            red = 100;
+            blue = 250;
+            green = 20;
             base.Initialize();
         }
 
@@ -28,6 +40,7 @@ namespace MiJuegoUno
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            spaceShip = this.Content.Load<Texture2D>("Spaceship");
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,15 +49,22 @@ namespace MiJuegoUno
                 Exit();
 
             // TODO: Add your update logic here
-
+            red++;
+            green++;
+            blue++;
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            //GraphicsDevice.Clear(new Color(red,green,blue));
+            GraphicsDevice.Clear(Color.Black);
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(spaceShip, new Vector2(300,250), Color.White);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
